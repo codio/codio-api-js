86e285e08e68f2272ab0c8bc52cec701bdf8b287
Andrey Nikitin <anikitin@codio.com>
2022-05-06 18:57:13 +0300

    update find course test

--- src/test/find-one-course.ts
+++ src/test/find-one-course.ts
@@ -7,7 +7,7 @@ const client_id = process.env.CLIENT_ID || '' // your clientId here
 const secret_id = process.env.SECRET_ID || '' // your secretId here
 
 const courseName = 'course 1'
-const courseId = 'e14d7ae7aa80059397886c7fcbe1b8f4'
+const courseId = 'e2842655b02e4620560fa753d8f5519a'
 
 const main = async () => {
     try {
@@ -16,8 +16,14 @@ const main = async () => {
         console.log('authed', authRes)
         const courseInfo = await codio.v1.course.info(courseId)
         console.log('course by id', courseInfo)
+        for (const unit of courseInfo) {
+            console.log('unit', unit.id, unit.name)
+            for (const assignment of unit.assignments) {
+                console.log('assignment', assignment.id, assignment.name)
+            }
+        }
 
-        const courseWithUnits = await codio.v1.course.findOneByName(courseName)
+        const courseWithUnits = await codio.v1.course.findOneByName(courseName, true)
         console.log('course by name', courseWithUnits)
         for (const unit of courseWithUnits.modules) {
             console.log('unit', unit.id, unit.name)
@@ -25,7 +31,6 @@ const main = async () => {
                 console.log('assignment', assignment.id, assignment.name)
             }
         }
-
     } catch (error: any) {
         if (error.json) {
             console.log(await error.json())

af6f0e32c853b68ff2bbaf35ad9751e383dc532e
ksimuk <maxim.kraev@gmail.com>
2022-05-10 10:49:25 +0100

    fix percent check

--- src/lib/assignment.ts
+++ src/lib/assignment.ts
@@ -236,7 +236,7 @@ function validatePenalty(penalty: Penalty) {
     throw new Error("penalty id must be a number and present")
   }
 
-  if (_.isNumber(penalty.percent) && penalty.percent >= 0 || penalty.percent <= 100) {
+  if (_.isNumber(penalty.percent) && (penalty.percent < 0 || penalty.percent > 100)) {
     throw new Error("penalty percent must be a number between 0 and 100")
   }
 

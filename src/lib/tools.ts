2e7b5fdcba46203cb6e86dde3ba4f1ef92b5ac54
achetporov <achetporov@codio.com>
2022-11-04 10:08:22 +0300

    clean

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -19,10 +19,10 @@ export async function reduce(
   const contentDir = path.join(srcDir, GUIDES_CONTENT_FOLDER)
   const rootMetadataPath = path.join(contentDir, INDEX_METADATA_FILE)
   const rootMetadata = readMetadataFile(rootMetadataPath)
-  const guidesStructure = getGuidesStructure(rootMetadata, srcDir, '');
-  const strippedStructure = stripStructure(guidesStructure, yaml_sections);
-  const excludePaths = [];
-  getExcludedPaths(guidesStructure, strippedStructure, excludePaths);
+  const guidesStructure = getGuidesStructure(rootMetadata, srcDir, '')
+  const strippedStructure = stripStructure(guidesStructure, yaml_sections)
+  const excludePaths = []
+  getExcludedPaths(guidesStructure, strippedStructure, excludePaths)
 
   await copyStripped(srcDir, dstDir, paths.concat(excludePaths))
   await updateRootMetadata(strippedStructure, rootMetadata, dstDir)
@@ -31,113 +31,113 @@ export async function reduce(
 
 function getGuidesStructure(metadata, srcDir, currentPath) {
   return _.map(metadata['order'], item => {
-    const contentDirPath = path.join(srcDir, GUIDES_CONTENT_FOLDER);
-    const sectionPath = path.join(currentPath, item);
-    const indexFilePath = path.join(contentDirPath, sectionPath, INDEX_METADATA_FILE);
+    const contentDirPath = path.join(srcDir, GUIDES_CONTENT_FOLDER)
+    const sectionPath = path.join(currentPath, item)
+    const indexFilePath = path.join(contentDirPath, sectionPath, INDEX_METADATA_FILE)
     if (!fs.existsSync(indexFilePath)) {
-      const metadataFilePath = path.join(contentDirPath, `${sectionPath}.json`);
+      const metadataFilePath = path.join(contentDirPath, `${sectionPath}.json`)
       const sectionMetadata = readMetadataFile(metadataFilePath)
-      sectionMetadata['name'] = item;
-      sectionMetadata['content_path'] = path.join(GUIDES_CONTENT_FOLDER, `${sectionPath}.md`);
-      sectionMetadata['metadata_path'] = path.join(GUIDES_CONTENT_FOLDER, `${sectionPath}.json`);
-      return sectionMetadata;
+      sectionMetadata['name'] = item
+      sectionMetadata['content_path'] = path.join(GUIDES_CONTENT_FOLDER, `${sectionPath}.md`)
+      sectionMetadata['metadata_path'] = path.join(GUIDES_CONTENT_FOLDER, `${sectionPath}.json`)
+      return sectionMetadata
     } else {
-      const newMetadataPath = path.join(currentPath, item);
-      const indexFilePath = path.join(contentDirPath, sectionPath, INDEX_METADATA_FILE);
+      const newMetadataPath = path.join(currentPath, item)
+      const indexFilePath = path.join(contentDirPath, sectionPath, INDEX_METADATA_FILE)
       const sectionMetadata = readMetadataFile(indexFilePath)
-      sectionMetadata['name'] = item;
-      sectionMetadata['metadata_path'] = path.join(GUIDES_CONTENT_FOLDER, newMetadataPath);
-      sectionMetadata['section_path'] = sectionPath;
-      sectionMetadata['children'] = getGuidesStructure(sectionMetadata, srcDir, sectionPath);
-      return sectionMetadata;
+      sectionMetadata['name'] = item
+      sectionMetadata['metadata_path'] = path.join(GUIDES_CONTENT_FOLDER, newMetadataPath)
+      sectionMetadata['section_path'] = sectionPath
+      sectionMetadata['children'] = getGuidesStructure(sectionMetadata, srcDir, sectionPath)
+      return sectionMetadata
     }
-  });
+  })
 }
 
 function readMetadataFile(path) {
   try {
-    const metadataJson = fs.readFileSync(path, {encoding: "utf-8"});
-    return JSON.parse(metadataJson);
+    const metadataJson = fs.readFileSync(path, {encoding: "utf-8"})
+    return JSON.parse(metadataJson)
   } catch (_) {
-    return [];
+    return []
   }
 }
 
 function stripStructure(guidesStructure, yaml_sections) {
-  const result: string[] = [];
+  const result: string[] = []
   const structure = _.cloneDeep(guidesStructure)
   for (const item of yaml_sections) {
     if (item.length === 0) { //skip empty sections
-      continue;
+      continue
     }
-    const section = traverseData(structure, item);
+    const section = traverseData(structure, item)
     if (!section) {
-      throw new Error(`${section} not found`);
+      throw new Error(`${section} not found`)
     }
-    result.push(section);
+    result.push(section)
   }
-  return result;
+  return result
 }
 
 function traverseData(structure, sections) {
-  const sectionName = sections.shift();
+  const sectionName = sections.shift()
   if (!sectionName) {
-    return;
+    return
   }
-  const section = findSection(structure, sectionName);
+  const section = findSection(structure, sectionName)
   if (!section) {
-    throw new Error(`section "${sectionName}" is not found`);
+    throw new Error(`section "${sectionName}" is not found`)
   }
   if (sections.length > 0) {
-    section['children'] = [traverseData(section.children, sections)];
-    return section;
+    section['children'] = [traverseData(section.children, sections)]
+    return section
   }
-  return section;
+  return section
 }
 
 function findSection(structure, title) {
-  const capitalTitle = _.upperCase(title);
+  const capitalTitle = _.upperCase(title)
   if (structure['type'] === 'page') {
     if (structure['title'] === capitalTitle) {
-      return structure;
+      return structure
     }
   }
   else {
     for (const item of structure) {
       if (_.upperCase(item['title']) === capitalTitle) {
-        return item;
+        return item
       }
     }
   }
-  return undefined;
+  return undefined
 }
 
 function getSectionIds(stripped) {
-  let ids: string[] = [];
+  let ids: string[] = []
   for (const item of stripped) {
     if (item['id']) {
-      ids.push(`${item.id}`);
+      ids.push(`${item.id}`)
     }
     if (item.children) {
       for (const section of [item.children]) {
-        const sectionIds = getSectionIds(section);
-        ids = ids.concat(sectionIds);
+        const sectionIds = getSectionIds(section)
+        ids = ids.concat(sectionIds)
       }
     }
   }
-  return ids;
+  return ids
 }
 
 function getExcludedPaths(structure, stripped, excludePaths) {
-  const ids = getSectionIds(stripped);
+  const ids = getSectionIds(stripped)
   for (const section of structure) {
     if (!ids.includes(section.id)) {
       if (section.type === 'page') {
-        excludePaths.push(`!${section.metadata_path}`);
-        excludePaths.push(`!${section.content_path}`);
+        excludePaths.push(`!${section.metadata_path}`)
+        excludePaths.push(`!${section.content_path}`)
       } else {
-        excludePaths.push(`!${section.metadata_path}`);
-        excludePaths.push(`!${section.metadata_path}/**`);
+        excludePaths.push(`!${section.metadata_path}`)
+        excludePaths.push(`!${section.metadata_path}/**`)
       }
     }
 
@@ -145,7 +145,7 @@ function getExcludedPaths(structure, stripped, excludePaths) {
       getExcludedPaths(section.children, stripped, excludePaths)
     }
   }
-  return excludePaths;
+  return excludePaths
 }
 
 async function copyStripped(srcDir: string, dstDir: string, paths: (string | PathMap)[]): Promise<void> {
@@ -156,7 +156,7 @@ async function copyStripped(srcDir: string, dstDir: string, paths: (string | Pat
   stringPaths.push('.codio-menu')
   stringPaths.push('.settings')
   stringPaths.push('!.github/**')
-  stringPaths.push('!**/index.json');
+  stringPaths.push('!**/index.json')
 
   _.forEach(_.filter(paths, _ => typeof _ === 'string') as string[],
           path => stringPaths.push(`${path}`))
@@ -181,22 +181,22 @@ async function copyStripped(srcDir: string, dstDir: string, paths: (string | Pat
 }
 
 async function updateRootMetadata(structure, metadata, dstDir) {
-  const filePath = path.join(dstDir, GUIDES_CONTENT_FOLDER, INDEX_METADATA_FILE);
-  metadata['order'] = _.map(structure, item => item.name);
-  await fs.promises.writeFile(filePath, JSON.stringify(metadata, undefined, ' '));
+  const filePath = path.join(dstDir, GUIDES_CONTENT_FOLDER, INDEX_METADATA_FILE)
+  metadata['order'] = _.map(structure, item => item.name)
+  await fs.promises.writeFile(filePath, JSON.stringify(metadata, undefined, ' '))
 }
 
 async function updateMetadata(structure, dstDir) {
   for (const item of structure) {
     if (item.children) {
-      const filePath = path.join(dstDir, GUIDES_CONTENT_FOLDER, item['section_path'], INDEX_METADATA_FILE);
+      const filePath = path.join(dstDir, GUIDES_CONTENT_FOLDER, item['section_path'], INDEX_METADATA_FILE)
       const data = {
         id: item.id,
         title: item.title,
         type: item.type,
         order: _.map(item.children, child => child.name)
       }
-      await fs.promises.writeFile(filePath, JSON.stringify(data, undefined, ' '));
+      await fs.promises.writeFile(filePath, JSON.stringify(data, undefined, ' '))
       await updateMetadata(item.children, dstDir)
     }
   }

bdd659fd39211a5577c4713b045f1cb215aa2c88
Andrey Nikitin <anikitin@codio.com>
2023-09-26 18:07:12 +0100

    add retries count

--- src/lib/bentWrapper.ts
+++ src/lib/bentWrapper.ts
@@ -4,6 +4,8 @@ interface Headers {
     [key: string]: any;
 }
 
+const RETRIES_COUNT = 5
+
 const waitTimeout = async (ms: number) => {
     await new Promise((resolve) => {
         setTimeout(resolve, ms)
@@ -12,8 +14,11 @@ const waitTimeout = async (ms: number) => {
 
 export default (...args: bent.Options[]) => {
     const api = bent(...args)
-    const req = async (url: string, body?: bent.RequestBody | undefined, headers?: Headers | undefined): Promise<any> => {
+    const req = async (url: string, body?: bent.RequestBody | undefined, headers?: Headers | undefined, tries?: number | undefined): Promise<any> => {
         try {
+            if (tries === 0) {
+                throw Error('Tries limit exceeded')
+            }
             const resp = await api(url, body, headers) as bent.NodeResponse
             return await resp.json()
         } catch (e: any) {
@@ -24,7 +29,8 @@ export default (...args: bent.Options[]) => {
                 }
                 const resetHeaderMs = e.headers['x-ratelimit-reset'] / 1000000
                 await waitTimeout(resetHeaderMs)
-                return await req(url, body, headers)
+                const triesCount = tries !== undefined && tries > 0 ? tries - 1 : RETRIES_COUNT
+                return await req(url, body, headers, triesCount)
             }
             throw e
         }

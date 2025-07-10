2968723dcdfde0bf464ca4fdab3a0088066f4335
Dmitrii Suchkov <dsuchkov@codio.com>
2025-07-10 11:02:11 +0100

    fix typo

--- examples/course/filterLearnersForMentors.js
+++ examples/course/filterLearnersForMentors.js
@@ -4,9 +4,8 @@ const { courseId, leanersMapping } = require('../data.js')
 async function main() {
     await auth
 
-    const compelted = await codio.course.filterLearnersForMentors(courseId, leanersMapping)
-    console.log('compelted', compelted)
-
+    const completed = await codio.course.filterLearnersForMentors(courseId, leanersMapping)
+    console.log('completed', completed)
 }
 
 main()

8bbf1e47ac8153babb74bd3c4cb2e2e86ca572d8
Sergei Bronnikov <sergeibronnikov87@gmail.com>
2026-07-29 12:09:47 +0100

    example

--- examples/course/deleteCourse.js
+++ examples/course/deleteCourse.js
@@ -3,7 +3,7 @@ const { codio, auth } = require('../auth.js')
 async function main() {
   await auth
   try {
-    await codio.course.deleteCourse(id, false)
+    await codio.course.deleteCourse('your course id', false)
     console.log("Course deleted successfully.")
   } catch (error) {
     console.error("Error deleting course:", error)
